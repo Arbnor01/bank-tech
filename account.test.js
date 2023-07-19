98ceedd22c469de81f05087409a59bc12b372d6b
Arbnor <arbnor-sfarca@hotmail.com>
2023-07-19 11:39:03 +0100

    Added test case for printStatement function

--- account.test.js
+++ account.test.js
@@ -26,6 +26,38 @@ describe('Account', () => {
     expect(account.transactions[1].date.toLocaleDateString('en-GB')).toEqual(expectedDate);
     expect(account.transactions[1]).toEqual({ date: debitDate, credit: '', debit: 500, balance: 500 });
   });
+
+  it('should print the statement with the correct format', () => {
+    const account = new Account();
+    const depositDate1 = new Date(2023, 0, 10);
+    account.deposit(1000, depositDate1);
+    const depositDate2 = new Date(2023, 0, 13);
+    account.deposit(2000, depositDate2);
+    const debitDate = new Date(2023, 0, 14);
+    account.debit(500, debitDate);
+  
+    // Capture the console output
+    const consoleOutput = [];
+    const originalLog = console.log;
+    console.log = jest.fn((...args) => {
+      consoleOutput.push(...args);
+      originalLog(...args);
+    });
+  
+    // Call the printStatement method
+    account.printStatement();
+  
+    // Restore the console.log function
+    console.log = originalLog;
+  
+    // Assert the expected output against the actual output
+    expect(consoleOutput).toEqual([
+      'Date || Credit || Debit || Balance',
+      '14/01/2023 ||  || 500.00 || 2500.00',
+      '13/01/2023 || 2000.00 ||  || 3000.00',
+      '10/01/2023 || 1000.00 ||  || 1000.00',
+    ]);
+  });
 });
 
 
